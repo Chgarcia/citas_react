70faef364d0bb519a7da44b651c1db83226b38e6
Christian García <christian.arizmendi@gmail.com>
2023-08-24 20:04:25 -0600

    lasst lite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     return(
       <>
         <h1 className="font-black text-5xl text-center  md:w-3/4 mx-auto">
-                Seguimiento Pacientes {" "} Con Github {' '}
+                Seguimiento Pacientes {" "} Con Github {' '} a la {' '}
                 <span className="text-indigo-500">Veterinaria</span>
         </h1>
       </>
