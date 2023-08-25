a99b261ee0501507f130979a340c76e35f4d0283
Christian García <christian.arizmendi@gmail.com>
2023-08-24 19:57:46 -0600

    No fue mucho trabajo pero fue un trabajo homesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     return(
       <>
         <h1 className="font-black text-5xl text-center  md:w-3/4 mx-auto">
-                Seguimiento Pacientes {" "} 
+                Seguimiento Pacientes {" "} Con Github {' '}
                 <span className="text-indigo-500">Veterinaria</span>
         </h1>
       </>
